1e0e5baf1802ca8abb004ef732265d7b491e1c40
Carlos Rodriguez <rodriguezcidcarlos@gmail.com>
2026-07-21 14:02:35 +0200

    Move core import after excel load

--- app.py
+++ app.py
@@ -2,34 +2,32 @@ print("000 APP START", flush=True)
 
 from pathlib import Path
 
-print("001 BEFORE DASH", flush=True)
-
 from dash import Dash, html
 
-print("002 DASH OK", flush=True)
+print("001 DASH OK", flush=True)
 
 from utils.io import load_excel_monthly
 
-print("003 IO OK", flush=True)
-
-from core.engine import AthleteOSCore
-
-print("004 CORE IMPORT OK", flush=True)
+print("002 IO OK", flush=True)
 
 
 BASE_DIR = Path(__file__).resolve().parent
 DATA_FILE = BASE_DIR / "data" / "synthetic_training.xlsx"
 
 
-print("005 BEFORE EXCEL", flush=True)
+print("003 BEFORE EXCEL", flush=True)
 
 df = load_excel_monthly(DATA_FILE)
 
-print("006 DATA OK", flush=True)
+print("004 DATA OK", flush=True)
 print(df.shape, flush=True)
 
 
-# PARAMOS AQUÍ
+from core.engine import AthleteOSCore
+
+print("005 CORE IMPORT OK", flush=True)
+
+
 app = Dash(__name__)
 
 app.layout = html.Div(
