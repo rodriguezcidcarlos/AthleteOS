e5d8dde144028e7a4cc2adf5f7e722ae42359d2e
Carlos Rodriguez <rodriguezcidcarlos@gmail.com>
2026-07-21 14:06:00 +0200

    Direct pandas excel read test

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 print("000 APP START", flush=True)
 
 from pathlib import Path
-
 from dash import Dash, html
 
 print("001 DASH OK", flush=True)
 
-from utils.io import load_excel_monthly
+import pandas as pd
 
-print("002 IO OK", flush=True)
+print("002 PANDAS OK", flush=True)
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -17,21 +16,26 @@ DATA_FILE = BASE_DIR / "data" / "synthetic_training.xlsx"
 
 print("003 BEFORE EXCEL", flush=True)
 
-df = load_excel_monthly(DATA_FILE)
+excel = pd.ExcelFile(DATA_FILE)
 
-print("004 DATA OK", flush=True)
+print("004 EXCEL OPEN OK", flush=True)
+
+print(excel.sheet_names, flush=True)
+
+
+df = pd.read_excel(
+    DATA_FILE
+)
+
+print("005 READ OK", flush=True)
+
 print(df.shape, flush=True)
-
-
-from core.engine import AthleteOSCore
-
-print("005 CORE IMPORT OK", flush=True)
 
 
 app = Dash(__name__)
 
 app.layout = html.Div(
-    "AthleteOS Render Test"
+    "AthleteOS Excel Test"
 )
 
 
